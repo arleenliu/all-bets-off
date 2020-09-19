a6e05c74286b704db1c7f7485459eb7df235cb60
Arleen Liu <arleenliu@gmail.com>
2020-09-19 15:51:41 -0700

    fix: what's going on

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -1,4 +1,5 @@
 const Challenge = require('../models/Challenge');
+const mongoose = require('mongoose');
 
 exports.getIndex = async (req, res) => {
     const challenge = await Challenge.find((data) => data);
@@ -32,7 +33,63 @@ exports.postChallenge = (req, res) => {
     const { group_id, challenge_name, info, end_condition, completed, prize } = req.body;
 
     const challenge = new Challenge({ group_id: group_id, challenge_name: challenge_name, info: info, end_condition: end_condition, completed: completed, prize: prize });
-    challenge.save();
-    console.log('Challenge Added to the database');
-    res.status(201).redirect('/');
+    var result;
+    Challenge.create(challenge, (err, c) => {
+        if (err) return `Error occurred while saving ${err}`;
+        const { _id } = c._id;
+        console.log(`New challenge id: ${_id}`);
+
+        result = c._id;
+        return _id;
+      });
+
+    res.status(201).send(result);
+    // res.status(201).redirect('/');
+};
+
+exports.postEditChallenge = (req, res) => {
+    const challengeId = req.body.challengeId;
+    const { group_id, challenge_name, info, end_condition, completed, prize } = req.body;
+
+    Challenge.findById(challengeId)
+        .then((challenge) => {
+            challenge.group_id = group_id;
+            challenge.challenge_name = challenge_name;
+            challenge.info = info;
+            challenge.end_condition = end_condition;
+            challenge.completed = completed;
+            challenge.prize = prize;
+
+            return challenge.save();
+        })
+        .then(() => {
+            console.log('Item Updated');
+            res.status(201).redirect('/');
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+};
+
+//unsure if necessary
+exports.getEditChallenge = async (req, res) => {
+    const challengeId = req.params.challengeId;
+
+    const editMode = req.query.edit;
+
+    if (!editMode) {
+        return res.redirect('/');
+    }
+
+    const challenge = await Challenge.findById(challengeId);
+
+    try {
+        if (!challengeId) {
+            return res.redirect('/');
+        }
+        console.log(challenge);
+        res.status(200).render('edit-challenge', { challenge: challenge, editing: editMode });
+    } catch (error) {
+        console.log(error);
+    }
 };
--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -9,6 +9,10 @@ router.get('/add-challenge', adminController.getAddChallenge);
 
 router.post('/add-challenge', adminController.postChallenge);
 
-router.get('/:challengeId', adminController.getAnime);
+router.get('/edit-challenge/:challengeId', adminController.getEditChallenge);
+
+router.get('/edit-challenge', adminController.postEditChallenge)
+
+router.get('get-challenge/:challengeId', adminController.getChallenge);
 
 module.exports = router;
